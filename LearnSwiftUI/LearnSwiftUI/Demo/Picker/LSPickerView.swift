4a152cddf1ec4fb20ad0c41aae3ca5260065b0b2
亓鑫 <qixin@sogou-inc.com>
2021-06-08 12:37:11 +0800

    - add Demo folder

---
+++ LearnSwiftUI/LearnSwiftUI/Demo/Picker/LSPickerView.swift
@@ -0,0 +1,20 @@
+//
+//  LSPickerView.swift
+//  LearnSwiftUI
+//
+//  Created by Qixin on 2021/6/8.
+//
+
+import SwiftUI
+
+struct LSPickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LSPickerView_Previews: PreviewProvider {
+    static var previews: some View {
+        LSPickerView()
+    }
+}
